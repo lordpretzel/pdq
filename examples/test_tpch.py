ceea48d0fc25cb65c09938dc8f352eadca96cb2a
subramanyaganesh <subramanya1174@gmail.com>
2024-02-04 19:43:57 -0600

    updating the pdq file and modified test_tpch.py

--- examples/test_tpch.py
+++ examples/test_tpch.py
@@ -2,8 +2,12 @@ import glob
 import os
 import subprocess
 import argparse
+import shutil
 
-q_dir = "tpchqs/tpcq"
+output_folder = './Generator_Folder'
+pdq_folder = './PDQ_Folder'
+q_dir = os.path.join(os.path.dirname(__file__), "tpchqs/tpcq")
+
 
 # Queries and the provenance tables we want to use
 queries = {
@@ -30,36 +34,51 @@ queries = {
     }
 
 query = ""
+python_executable = shutil.which('python') or shutil.which('python3.9')
 
-cmd1 = ["python","query-and-schema-generator.py","translate_sql_and_query_and_prov", "-i", "./tpchqs/tpch.sql", "-o", "./tpchqs/sout.xml", "-P", "-f", "--replace_dt",  "--query_file", "./tpchqs/qout.xml", "-q"]
+cmd1 = [python_executable,"query-and-schema-generator.py","translate_sql_and_query_and_prov", "-i", "./tpchqs/tpch.sql", "-o", "./tpchqs/sout.xml", "-P", "-f", "--replace_dt",  "--query_file", "./tpchqs/qout.xml", "-q"]
 
 cmd2 = ["java", "-jar", "pdq-main-2.0.0-executable.jar", "planner", "-s", "./tpchqs/sout.xml", "-q", "./tpchqs/qout.xml"]
 
+def run_pdq():
+    run_cmd = subprocess.Popen(cmd2, stdout=subprocess.PIPE)
+    output = run_cmd.communicate()[0].decode("utf-8")
+
+    if not os.path.exists(pdq_folder):
+        os.makedirs(pdq_folder)
+        output_file_path = os.path.join(pdq_folder, "pdq_Output.txt")
+        with open(output_file_path, 'w') as output_file:
+                    output_file.write(output)
+    
+
 def run_all():
-    for qi in queries:
-        run_single(qi)
+    for index,qi in enumerate(queries, start=1) :
+        run_single(qi,index)
 #    flist = glob.glob(q_dir + "{:02d}/tpcq{:02d}.dl".format(d,d))
         
-def run_single(qi, run_pdq=False):
+def run_single(qi,index,run_pdq=False):
     fn = q_dir + qi + "/tpcq"+qi+".dl"
-    print(fn)
     with open(fn) as f:
         lines = [line for line in f.readlines() if line.strip()]
         query = lines[0].rstrip()
-#        query = '"' + query + '"'
-        gen_command = cmd1 + [query] + ["--prov"] + [queries[qi][0]]
-        print(gen_command)
-        
-        run_cmd = subprocess.Popen(gen_command, stdout=subprocess.PIPE)
-        output = run_cmd.communicate()[0].decode("utf-8")
-        print(output)
-        
-        if run_pdq:
-            run_cmd = subprocess.Popen(cmd2, stdout=subprocess.PIPE)
+        print("\n\nRunning query", queries[qi])
+        for element in queries[qi]:
+            gen_command = cmd1 + [query] + ["--prov"] + [element]
+            run_cmd = subprocess.Popen(gen_command, stdout=subprocess.PIPE)
             output = run_cmd.communicate()[0].decode("utf-8")
-            print(output)
+           
+            if not os.path.exists(output_folder):
+                os.makedirs(output_folder)
+            output_file_path = os.path.join(output_folder, f"output_{index}_{element}.txt")
+            with open(output_file_path, 'w') as output_file:
+                output_file.write(output)
+            if run_pdq:
+                run_pdq()
+                
         
 def main():
+    if os.path.exists(output_folder):
+        shutil.rmtree(output_folder)
     parser=argparse.ArgumentParser(description="Run tcp_h tests on pdq.")
     parser.add_argument('-i', '--individual', type=int, required=False, metavar="[1-20]", choices=range(1, 21))
     parser.add_argument('-r', '--run', action='store_true')
