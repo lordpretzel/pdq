b2d772217ca1c556c2898e9237732f0f9faf19f5
subramanyaganesh <subramanya1174@gmail.com>
2024-02-09 16:37:22 -0600

    template1

--- examples/test_tpch.py
+++ examples/test_tpch.py
@@ -6,6 +6,9 @@ import shutil
 
 output_folder = './Generator_Folder'
 pdq_folder = './PDQ_Folder'
+sout_folder = './SOUT_Folder'
+qout_folder = './QOUT_Folder'
+timeoutPDQ = 60
 q_dir = os.path.join(os.path.dirname(__file__), "tpchqs/tpcq")
 
 
@@ -36,49 +39,85 @@ queries = {
 query = ""
 python_executable = shutil.which('python') or shutil.which('python3.9')
 
-cmd1 = [python_executable,"query-and-schema-generator.py","translate_sql_and_query_and_prov", "-i", "./tpchqs/tpch.sql", "-o", "./tpchqs/sout.xml", "-P", "-f", "--replace_dt",  "--query_file", "./tpchqs/qout.xml", "-q"]
+# "-o", "./tpchqs/sout.xml",
+# "--query_file", "./tpchqs/qout.xml", 
+cmd1 = [python_executable,"query-and-schema-generator.py","translate_sql_and_query_and_prov", "-i", "./tpchqs/tpch.sql",  "-P", "-f", "--replace_dt",  "-q"]
 
-cmd2 = ["java", "-jar", "pdq-main-2.0.0-executable.jar", "planner", "-s", "./tpchqs/sout.xml", "-q", "./tpchqs/qout.xml"]
+# "-s", "./tpchqs/sout.xml",
+# "-q", "./tpchqs/qout.xml"
+cmd2 = ["java", "-jar", "pdq-main-2.0.0-executable.jar", "planner"  ]
 
-def run_pdq():
-    run_cmd = subprocess.Popen(cmd2, stdout=subprocess.PIPE)
-    output = run_cmd.communicate()[0].decode("utf-8")
+def run_pdq(element,index):
+    print(f"Running PDQ for {index}:{element}")
+    try:
+        sout_file_path = os.path.join(sout_folder, f"sout_{index}_{element}.xml")
+        qout_file_path = os.path.join(qout_folder, f"qout_{index}_{element}.xml")
+        gen_command = cmd2 +["-s"] + [sout_file_path]+ ["-q"] + [qout_file_path]
+        run_cmd = subprocess.Popen(gen_command, stdout=subprocess.PIPE)
+        output, error = run_cmd.communicate(timeout=timeoutPDQ)  
+        output = output.decode("utf-8")
+        print("!!!!!",error)
+        if error:
+            output += "\n--------------[ERRORS]---------------\n" + error
 
-    if not os.path.exists(pdq_folder):
-        os.makedirs(pdq_folder)
-        output_file_path = os.path.join(pdq_folder, "pdq_Output.txt")
-        with open(output_file_path, 'w') as output_file:
-                    output_file.write(output)
-    
+    except subprocess.TimeoutExpired:
+        print("in time out")
+        run_cmd.kill() 
+        output = f"Process timed out after {timeoutPDQ} seconds."
+    finally:
+        print("PDQ finally:")
+        if not os.path.exists(pdq_folder):
+            os.makedirs(pdq_folder)
+        pdq_file_path = os.path.join(pdq_folder, f"PDQ_{index}_{element}.txt")
+        print("PDQ file path:", pdq_file_path)
+        with open(pdq_file_path, 'w') as output_file:
+            output_file.write(output)
+
 
 def run_all():
     for index,qi in enumerate(queries, start=1) :
         run_single(qi,index)
-#    flist = glob.glob(q_dir + "{:02d}/tpcq{:02d}.dl".format(d,d))
         
-def run_single(qi,index,run_pdq=False):
+def run_single(qi,index,run_pdqs=True):
     fn = q_dir + qi + "/tpcq"+qi+".dl"
     with open(fn) as f:
         lines = [line for line in f.readlines() if line.strip()]
         query = lines[0].rstrip()
-        print("\n\nRunning query", queries[qi])
-        for element in queries[qi]:
-            gen_command = cmd1 + [query] + ["--prov"] + [element]
-            run_cmd = subprocess.Popen(gen_command, stdout=subprocess.PIPE)
-            output = run_cmd.communicate()[0].decode("utf-8")
-           
-            if not os.path.exists(output_folder):
-                os.makedirs(output_folder)
-            output_file_path = os.path.join(output_folder, f"output_{index}_{element}.txt")
-            with open(output_file_path, 'w') as output_file:
-                output_file.write(output)
-            if run_pdq:
-                run_pdq()
+        print("\n\nRunning query",index,":",queries[qi][0])
+        # for element in queries[qi]:
+        element = queries[qi][0]
+
+        sout_file_path = os.path.join(sout_folder, f"sout_{index}_{element}.xml")
+
+        qout_file_path = os.path.join(qout_folder, f"qout_{index}_{element}.xml")
+
+        gen_command = cmd1 + [query] + ["--prov"] + [element] + ["-o"]+ [sout_file_path] + ["--query_file"] + [qout_file_path]
+        run_cmd = subprocess.Popen(gen_command, stdout=subprocess.PIPE)
+        output,error = run_cmd.communicate()
+        output = output.decode("utf-8")
+        error = error
+        if error:
+            output += "\n----[ERRORS]-----\n" + error
+        
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        output_file_path = os.path.join(output_folder, f"output_{index}_{element}.txt")
+        with open(output_file_path, 'w') as output_file:
+            output_file.write(output)
+            
+
+    if run_pdqs:
+            run_pdq(element,index)
                 
         
 def main():
-    if os.path.exists(output_folder):
-        shutil.rmtree(output_folder)
+    folders_to_check = [output_folder, pdq_folder,sout_folder,qout_folder]
+    for folder in folders_to_check:
+        if os.path.exists(folder):
+            shutil.rmtree(folder)
+    
+    os.makedirs(sout_folder)
+    os.makedirs(qout_folder)
     parser=argparse.ArgumentParser(description="Run tcp_h tests on pdq.")
     parser.add_argument('-i', '--individual', type=int, required=False, metavar="[1-20]", choices=range(1, 21))
     parser.add_argument('-r', '--run', action='store_true')
